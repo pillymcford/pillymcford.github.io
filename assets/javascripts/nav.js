0dd3c68cb0b087996fd48c7f6a31072d560335e2
jersey-griggs <39424338+jersey-griggs@users.noreply.github.com>
2020-10-30 00:42:52 -0400

    nav changes

--- assets/javascripts/nav.js
+++ assets/javascripts/nav.js
@@ -1,17 +1,25 @@
 let scrollpos = window.scrollY
-const header = document.querySelector("nav")
-const header_height = header.offsetHeight
+const nav = document.querySelector("nav")
+const header = document.querySelector("header")
+const nav_height = nav.offsetHeight
 
-const add_class_on_scroll = () => header.classList.add("shrink-nav")
-const remove_class_on_scroll = () => header.classList.remove("shrink-nav")
+const add_class_on_scroll = () => nav.classList.add("shrink-nav")
+const remove_class_on_scroll = () => nav.classList.remove("shrink-nav")
+
+const remove_class_top = () => nav.classList.remove("nav-top")
+const add_class_top = () => nav.classList.add("nav-top")
+
+
 
 window.addEventListener('scroll', function() {
   scrollpos = window.scrollY;
 
-  if (scrollpos >= header_height) {
+  if (scrollpos >= nav_height) {
     add_class_on_scroll()
+    remove_class_top()
    }
   else {
     remove_class_on_scroll()
+    add_class_top()
   }
 })
